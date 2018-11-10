a08121e76956a0285d7f1459bf704f5d8138a81b
Jonathan Hager <jhager@gmail.com>
2018-11-10 13:31:34 -0800

    renamed key for shuffler

--- src/components/Profile.jsx
+++ src/components/Profile.jsx
@@ -34,7 +34,7 @@ export default class Profile extends Component {
 
      let newGame = {
        id: uuidv4(),
-       text: shufflerBytes,
+       shuffler: shufflerBytes,
        created_at: Date.now(),
        opponent: opponentId
      }
