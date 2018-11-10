8b47d1344340db242a38f69b8566f0ff67740bbb
Jonathan Hager <jhager@gmail.com>
2018-11-10 13:29:25 -0800

    added saving game state to gaia

--- src/components/Profile.jsx
+++ src/components/Profile.jsx
@@ -8,6 +8,7 @@ import {
   lookupProfile,
 } from 'blockstack';
 
+const uuidv4 = require('uuid/v4');
 const avatarFallbackImage = 'https://s3.amazonaws.com/onename/avatar-placeholder.png';
 export default class Profile extends Component {
   constructor(props) {
@@ -27,6 +28,26 @@ export default class Profile extends Component {
   isLocal() {
     return this.props.match.params.username ? false : true
   }
+
+  saveGameResults(shufflerBytes, opponentId) {
+     let allGames = this.state.games || [];
+
+     let newGame = {
+       id: uuidv4(),
+       text: shufflerBytes,
+       created_at: Date.now(),
+       opponent: opponentId
+     }
+
+     allGames.unshift(newGame)
+     const options = { encrypt: false }
+     putFile('high-cards.json', JSON.stringify(allGames), options)
+       .then(() => {
+         this.setState({
+           games: allGames
+         })
+       })
+   }
 
   render() {
    const { handleSignOut } = this.props;
@@ -72,10 +93,15 @@ export default class Profile extends Component {
   fetchData() {
     if (this.isLocal()) {
       const options = { decrypt: false }
-      this.setState({
-        person: new Person(loadUserData().profile),
-        username: loadUserData().username
-      })
+      getFile('high-cards.json', options)
+        .then((file) => {
+          var games = JSON.parse(file || '[]')
+          this.setState({
+            person: new Person(loadUserData().profile),
+            username: loadUserData().username,
+            games: games
+          })
+        })
     }
     else {
       const username = this.props.match.params.username
@@ -92,9 +118,25 @@ export default class Profile extends Component {
       .finally(() => {
         this.setState({ isLoading: false })
       })
+
+      const options = { username: username, decrypt: false }
+      getFile('high-cards.json', options)
+      .then((file) => {
+         var deseralizedGames = JSON.parse(file || '[]')
+         this.setState({
+           games: deseralizedGames
+         })
+      })
+      .catch((error) => {
+          console.log('could not fetch games')
+      })
+  .finally(() => {
+    this.setState({ isLoading: false })
+  })
     }
   }
   componentDidMount() {
     this.fetchData()
+    //this.saveGameResults("D95849AA570A03231B69C65749054A6C8770C7345F8673EC12FA94B302A1ECCC9A90AF79B356FE648479EA59AF6BE7094AD323C3AC4B7373016D6A078625BF43", "jhager.id.blockstack")
   }
 }
