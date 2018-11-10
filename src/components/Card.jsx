06fc53f08d5def362bd8d0663a31a7def88842c6
Cole Albon <calbon@cbtnuggets.com>
2018-11-10 14:11:15 -0800

    add card component:

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -1,81 +1,11 @@
-export default class Profile extends Component {
-  constructor(props) {
-  	super(props);
-  	this.state = {
-  	};
-  }
-
-  isLocal() {
-    return this.props.match.params.username ? false : true
-  }
-
+import React, { Component } from 'react';
+export default class Card extends Component {
   render() {
-   const { handleSignOut } = this.props;
-   const { person } = this.state;
-   const { username } = this.state;
-
    return (
-     !isSignInPending() && person ?
-     <div className="container">
-       <div className="row">
-         <div className="col-md-offset-3 col-md-6">
-           <div className="col-md-12">
-             <div className="avatar-section">
-               <img
-                 src={ person.avatarUrl() ? person.avatarUrl() : avatarFallbackImage }
-                 className="img-rounded avatar"
-                 id="avatar-image"
-               />
-               <div className="username">
-                 <h1>
-                   <span id="heading-name">{ person.name() ? person.name()
-                     : 'Nameless Person' }</span>
-                 </h1>
-                 <span>{username}</span>
-                 {this.isLocal() &&
-                   <span>
-                     &nbsp;|&nbsp;
-                     <a onClick={ handleSignOut.bind(this) }>(Logout)</a>
-                   </span>
-                 }
-               </div>
-             </div>
-           </div>
-           {
-             this.isLocal()
-           }
-         </div>
-       </div>
-     </div> : null
+    <div>
+    { this.props.suit }
+    { this.props.size }
+    </div>
    );
-  }
-
-  fetchData() {
-    if (this.isLocal()) {
-      const options = { decrypt: false }
-      this.setState({
-        person: new Person(loadUserData().profile),
-        username: loadUserData().username
-      })
-    }
-    else {
-      const username = this.props.match.params.username
-      lookupProfile(username)
-      .then((profile) => {
-        this.setState({
-          person: new Person(profile),
-          username: username
-        })
-      })
-      .catch((error) => {
-        console.log('could not resolve profile')
-      })
-      .finally(() => {
-        this.setState({ isLoading: false })
-      })
-    }
-  }
-  componentDidMount() {
-    this.fetchData()
   }
 }
--- src/components/Game.jsx
+++ src/components/Game.jsx
@@ -1,4 +1,6 @@
 import React, { Component } from 'react';
+import Card from './Card.jsx';
+
 export default class Game extends Component {
 
   constructor(props) {
@@ -16,8 +18,8 @@ export default class Game extends Component {
    return (
     <div>
     <br></br>
-    our card: { ourCard } &nbsp;
-    their card: { theirCard }
+    our card: <Card size='4' suit='h'></Card> &nbsp;
+    their card:  <Card size='5' suit='S'></Card>
     </div>
    );
   }
