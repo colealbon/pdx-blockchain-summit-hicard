6698bd5ef8cd1b1180c10c495a2ea8d096f9a4b8
Jonathan Hager <jhager@gmail.com>
2018-11-11 00:10:49 -0800

    changed to using a CORS enabled enpoint

--- src/components/Game.jsx
+++ src/components/Game.jsx
@@ -5,6 +5,7 @@ import shuffle from '../lib/shuffle.js'
 
 let beacon = require('nist-randomness-beacon')
 
+import axios from 'axios';
 
 
 export default class Game extends Component {
@@ -70,8 +71,8 @@ export default class Game extends Component {
   }
 
   reevaluateCard(rando) {
-
     shuffle(this.state.deck, rando.localRandomValue)
+
     this.setState({
       ourCard: this.state.deck[0],
       theirCard: this.state.deck[1]
@@ -81,27 +82,25 @@ export default class Game extends Component {
   componentWillMount() {
     let timestamp = Date.now();
 
-    const rando = {
-      localRandomValue: '979142837A30C60B7E84351B37626F742197474E23E349D5287C82C63ABF62C3AA272BBB1D013C5EDECB2184E2EB8A901C70DCF59F10A75371E11FB1BB7F21A9'.split('').sort(function(){return 0.5-Math.random()}).join(''),
-      pulseIndex: 154703
-    }
 
-    this.reevaluateCard(rando)
+    //1 was at 2018-07-23T19:26:00.000Z (1532373960)
+    //155238 was at 2018-11-11T07:53:00.000Z (1541922780)
+    //1541922780/60 - 1532373960/60 ==> 159147
+    //so there are some lost minutes.  (about 1.5 seconds every minute)
+    //159147-155238
 
-    /*
-    beacon // unusable in a browser
-    .getMostRecentPulse()
-    .then(res => {
-        this.setState({
-          localRandomValue: this.pulse.localRandomValue,
-          pulseIndex: this.pulse.pulseIndex
+    let guess = Math.floor((timestamp/1000 - 1541922780)/61.5) + 155238
+
+    axios.get('https://s3.amazonaws.com/nist-beacons/' + guess + '.json')
+      .then(res => {
+        const json = res.data;
+
+        this.reevaluateCard({
+          localRandomValue: json.pulse.localRandomValue,
+          pulseIndex: json.pulse.pulseIndex
         })
-
-    })
-    .catch(err => {
-      console.error(err)
-    })
-    */
+      })
+      
   }
   render() {
    const { ourCard } = this.state;
@@ -110,10 +109,12 @@ export default class Game extends Component {
    return (
     <div>
     <br></br>
-    our card: <Card size={ourCard.size} suit={ourCard.suit}></Card> &nbsp;
-    their card:  <Card size={theirCard.size} suit={theirCard.suit}></Card>
+    our card: <Card size={ourCard == null ? "" : ourCard.size} suit={ourCard == null ? "" : ourCard.suit}></Card> &nbsp;
+    their card:  <Card size={theirCard == null ? "" :  theirCard.size} suit={theirCard == null ? "" : theirCard.suit}></Card>
     <br></br>
-    winner:  {(theirCard.val == ourCard.val) ? "tie" : ((theirCard.val > ourCard.val) ? 'them' : 'us') }
+      {theirCard && ourCard &&
+        <span>winner:  {(theirCard.val == ourCard.val) ? 'tie' : ((theirCard.val > ourCard.val) ? 'them' : 'us') }</span>
+      }
     </div>
    );
   }
